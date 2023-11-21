1065c26061e64ff8428bf20560ae39078537e842
Kavya <kavyadhawan22@yahoo.com>
2023-11-21 21:38:46 +0300

    modified:   sc.pl

--- sc.pl
+++ sc.pl
@@ -1,1 +1,22 @@
-% source code for expert system
+% raw data
+
+% T2: Farsightedness
+farsightedness('yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'Confidence level:', '95%')
+farsightedness('yes', 'yes', 'yes', 'yes', 'yes', 'no', 'yes', 'yes', 'Confidence level:', '93.75%')
+farsightedness('yes', 'yes', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'Confidence level:', '93.75%')
+farsightedness('yes', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'Confidence level:', '92.5%')
+farsightedness('yes', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', 'Confidence level:', '93.75%')
+farsightedness('yes', 'yes', 'no', 'yes', 'yes', 'no', 'yes', 'yes', 'Confidence level:', '92.5%')
+farsightedness('yes', 'yes', 'no', 'no', 'yes', 'yes', 'yes', 'yes', 'Confidence level:', '92.5%')
+farsightedness('yes', 'yes', 'no', 'no', 'yes', 'no', 'yes', 'yes', 'Confidence level:', '91.25%')
+
+
+% T4: Narsightedness
+nearsightedness('yes', 'yes', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'Confidence level:', '95%')
+nearsightedness('yes', 'yes', 'yes', 'yes', 'no', 'no', 'no', 'yes', 'yes', 'Confidence level:', '93.75%')
+nearsightedness('yes', 'yes', 'yes', 'no', 'no', 'yes', 'no', 'yes', 'yes', 'Confidence level:', '93.75%')
+nearsightedness('yes', 'yes', 'yes', 'no', 'no', 'no', 'no', 'yes', 'yes', 'Confidence level:', '92.5%')
+nearsightedness('yes', 'yes', 'no', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'Confidence level:', '93.75%')
+nearsightedness('yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'yes', 'Confidence level:', '92.5%')
+nearsightedness('yes', 'yes', 'no', 'no', 'no', 'yes', 'no', 'yes', 'yes', 'Confidence level:', '92.5%')
+nearsightedness('yes', 'yes', 'no', 'no', 'no', 'no', 'no', 'yes', 'yes', 'Confidence level:', '91.25%')
